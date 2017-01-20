2f1abbaa64a171fb9d55389f8d6aa4633d73b6e0
Sarah Allen <sarah@zooniverse.org>
2017-01-20 15:04:20 -0600

    build cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export { default as UserSearch } from './components/user-search';
 export { default as Thumbnail } from './components/thumbnail';
 export { default as MediaIcon } from './components/media-icon';
-export { default as ImageSelector } from './components/image-selector';
 export { default as FileButton } from './components/file-button';
 export { default as DragAndDropTarget } from './components/drag-and-drop-target';
 export { default as ZooniverseLogotype } from './components/zooniverse-logotype';
