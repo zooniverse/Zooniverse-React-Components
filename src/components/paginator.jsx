8bfd971c180091c732d01d647b0d3632b85b7474
mcbouslog <mcbouslog@gmail.com>
2017-05-25 16:48:37 -0500

    update styling

--- src/components/paginator.jsx
+++ src/components/paginator.jsx
@@ -6,83 +6,112 @@ const pageOption = (n, i) =>
   </option>;
 
 const Paginator = ({
+  className,
   firstAndLast,
+  itemCount,
   nextLabel,
+  // onClickNext,
+  // onClickPrev,
   onPageChange,
   page,
   pageCount,
   pageSelector,
   previousLabel,
-}) =>
-  <div className="paginator">
-    {firstAndLast &&
-      (<button
-        type="button"
-        className="paginator-first"
-        onClick={() => onPageChange(1)}
-        disabled={page === 1}
-      >
-        <i className="fa fa-fast-backward" /> First
-      </button>)}
+  totalItems,
+}) => {
+  const paginatorClassName = `paginator ${className}`.trim();
 
-    <button
-      type="button"
-      className="paginator-prev"
-      disabled={page === 1}
-      onClick={() => onPageChange(page - 1)}
-    >
-      {previousLabel}
-    </button>
-
-    {pageSelector &&
-      (<div className="paginator-page-selector">
-        Page&nbsp;
-        <select
-          value={page}
-          onChange={(e) => { onPageChange(e.target.value); }}
+  return (
+    <div className={paginatorClassName}>
+      {firstAndLast &&
+        (<button
+          type="button"
+          className="paginator-button"
+          onClick={() => onPageChange(1)}
+          disabled={page === 1}
         >
-          {Array.from({ length: pageCount }, (v, i) => i + 1).map(pageOption)}
-        </select> of {pageCount}
-      </div>)}
+          <i className="fa fa-fast-backward" /> First
+        </button>)}
 
-    <button
-      type="button"
-      className="paginator-next"
-      disabled={page === pageCount}
-      onClick={() => onPageChange(page + 1)}
-    >
-      {nextLabel}
-    </button>
-
-    {firstAndLast &&
-      (<button
+      <button
         type="button"
-        className="paginator-last"
-        onClick={() => onPageChange(pageCount)}
-        disabled={page === pageCount}
+        className="paginator-button"
+        disabled={page === 1}
+        onClick={() => onPageChange(page - 1)}
       >
-        Last <i className="fa fa-fast-forward" />
-      </button>)}
+        {previousLabel}
+      </button>
 
-  </div>;
+      {pageSelector &&
+        (<div className="paginator-page-selector">
+          Page&nbsp;
+          <select
+            value={page}
+            onChange={(e) => { onPageChange(e.target.value); }}
+          >
+            {Array.from({ length: pageCount }, (v, i) => i + 1).map(pageOption)}
+          </select> of {pageCount}
+        </div>)}
+
+      {itemCount && totalItems &&
+        (<div className="paginator-page-selector">
+          {totalItems}
+        </div>)}
+
+      <button
+        type="button"
+        className="paginator-button"
+        disabled={page === pageCount}
+        onClick={() => onPageChange(page + 1)}
+      >
+        {nextLabel}
+      </button>
+
+      {firstAndLast &&
+        (<button
+          type="button"
+          className="paginator-button"
+          onClick={() => onPageChange(pageCount)}
+          disabled={page === pageCount}
+        >
+          Last <i className="fa fa-fast-forward" />
+        </button>)}
+
+    </div>
+  );
+};
 
 Paginator.defaultProps = {
+  className: '',
   firstAndLast: true,
-  itemCount: true,
+  itemCount: false,
   nextLabel: <span>Next <i className="fa fa-long-arrow-right" /></span>,
+  // onClickNext: () => {
+  //   this.onPageChange(this.page + 1);
+  // },
+  // onClickPrev: () => {
+  //   this.onPageChange(this.page - 1);
+  // },
+  onPageChange: () => {},
   page: 1,
   pageSelector: true,
   previousLabel: <span><i className="fa fa-long-arrow-left" /> Previous</span>,
+  totalItems: undefined,
 };
 
 Paginator.propTypes = {
+  className: React.PropTypes.string,
   firstAndLast: React.PropTypes.bool,
+  itemCount: React.PropTypes.bool,
   nextLabel: React.PropTypes.node,
+  // onClickNext: React.PropTypes.func,
+  // onClickPrev: React.PropTypes.func,
   onPageChange: React.PropTypes.func.isRequired,
-  page: React.PropTypes.number,
+  page: React.PropTypes.number.isRequired,
   pageCount: React.PropTypes.number,
   pageSelector: React.PropTypes.bool,
   previousLabel: React.PropTypes.node,
+  totalItems: React.PropTypes.node,
 };
 
 export default Paginator;
